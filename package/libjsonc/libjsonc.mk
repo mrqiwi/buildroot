6f6beedf6610644c97886c260297cf59e5a834d8
mrqiwi <mrqiwi@ya.ru>
2020-10-28 14:52:02 +0300

    remove dlink define

--- package/libjsonc/libjsonc.mk
+++ package/libjsonc/libjsonc.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 LIBJSONC_VERSION = d0b87ee
-# LIBJSONC_SITE = https://github.com/json-c/json-c.git
-LIBJSONC_SITE = $(DLINK_STORAGE)
+LIBJSONC_SITE = https://github.com/json-c/json-c.git
 LIBJSONC_SOURCE = json-c-$(LIBJSONC_VERSION).tar.gz
 LIBJSONC_LICENSE = MIT
 LIBJSONC_LICENSE_FILES = COPYING
